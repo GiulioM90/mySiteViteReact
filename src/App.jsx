7c8a51b5e9bd15f9850ac9574505cb0109fb16b4
gmoll <gmollo@wheretech.it>
2023-03-04 00:12:08 +0100

    nav completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-
+import { BrowserRouter } from "react-router-dom";
+import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
+// https://www.youtube.com/watch?v=0fYi8SGA20k&list=WL&index=24&t=218s
+// Build and Deploy an Amazing 3D Web Developer Portfolio in React JS | Beginner Three.js 
+// 40:50
 function App() {
-
   return (
-   <div>
-    3D Developer Portfolio
-   </div>
+   <BrowserRouter>
+    <div className="relative z-0 bg-primary">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <Navbar />
+        <Hero />
+      </div>
+      <About/>
+      <Experience/>
+      <Tech/>
+      <Works/>
+      <Feedbacks/>
+      <div className="relative z-0">
+        <Contact/>
+        <StarsCanvas/>
+      </div>
+    </div>
+   </BrowserRouter>
   )
 }
 
